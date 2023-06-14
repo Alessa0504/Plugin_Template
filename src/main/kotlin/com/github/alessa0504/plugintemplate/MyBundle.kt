dc42f51254e8867976492149f9e3248c74c25750
GitHub Action <action@github.com>
2023-06-14 11:45:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/alessa0504/plugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/alessa0504/plugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alessa0504.plugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/alessa0504/plugintemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/alessa0504/plugintemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alessa0504.plugintemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
